f014586f85410c67acc4fbedeea6c47a5d82f081
yrfriend <cureoglu1@gmail.com>
2021-05-31 20:56:17 +0300

    stuff

--- main.go
+++ main.go
@@ -2,64 +2,31 @@ package main
 
 import (
 	"crypto/tls"
-	"fmt"
+	"log"
 	"net/http"
-	"path/filepath"
 
 	"golang.org/x/crypto/acme/autocert"
 )
 
-var (
-	domain string
-)
-
-func getSelfSignedOrLetsEncryptCert(certManager *autocert.Manager) func(hello *tls.ClientHelloInfo) (*tls.Certificate, error) {
-	return func(hello *tls.ClientHelloInfo) (*tls.Certificate, error) {
-		dirCache, ok := certManager.Cache.(autocert.DirCache)
-		if !ok {
-			dirCache = "certs"
-		}
-
-		keyFile := filepath.Join(string(dirCache), hello.ServerName+".key")
-		crtFile := filepath.Join(string(dirCache), hello.ServerName+".crt")
-		certificate, err := tls.LoadX509KeyPair(crtFile, keyFile)
-		if err != nil {
-			fmt.Printf("%s\nFalling back to Letsencrypt\n", err)
-			return certManager.GetCertificate(hello)
-		}
-		fmt.Println("Loaded selfsigned certificate.")
-		return &certificate, err
-	}
-}
-
 func main() {
-	//flag.StringVar(&domain, "domain", "", "domain name to request your certificate")
-	//flag.Parse()
-
-	domain := "167.71.16.181"
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello HTTP/2")
-	})
-
-	fmt.Println("TLS domain", domain)
 	certManager := autocert.Manager{
 		Prompt:     autocert.AcceptTOS,
-		HostPolicy: autocert.HostWhitelist(domain),
-		Cache:      autocert.DirCache("certs"),
+		HostPolicy: autocert.HostWhitelist("167.71.16.181"), //Your domain here
+		Cache:      autocert.DirCache("certs"),              //Folder for storing certificates
 	}
 
-	tlsConfig := certManager.TLSConfig()
-	tlsConfig.GetCertificate = getSelfSignedOrLetsEncryptCert(&certManager)
-	server := http.Server{
-		Addr:      ":443",
-		Handler:   mux,
-		TLSConfig: tlsConfig,
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello world"))
+	})
+
+	server := &http.Server{
+		Addr: ":https",
+		TLSConfig: &tls.Config{
+			GetCertificate: certManager.GetCertificate,
+		},
 	}
 
-	go http.ListenAndServe(":80", certManager.HTTPHandler(nil))
-	fmt.Println("Server listening on", server.Addr)
-	if err := server.ListenAndServeTLS("", ""); err != nil {
-		fmt.Println(err)
-	}
+	go http.ListenAndServe(":http", certManager.HTTPHandler(nil))
+
+	log.Fatal(server.ListenAndServeTLS("", "")) //Key and cert are coming from Let's Encrypt
 }
