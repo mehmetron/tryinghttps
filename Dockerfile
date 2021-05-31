40ace0edef60afa88b7afb29acf43cc36e74ba1e
yrfriend <cureoglu1@gmail.com>
2021-05-31 21:23:47 +0300

    stuff

--- Dockerfile
+++ Dockerfile
@@ -7,3 +7,17 @@ RUN CGO_ENABLED=0 go build -o /bin/demo
 FROM scratch
 COPY --from=build /bin/demo /bin/demo
 ENTRYPOINT ["/bin/demo"]
+
+
+# Take  from https://tutorialedge.net/golang/go-docker-tutorial/
+#FROM golang:1.12.0-alpine3.9
+#RUN mkdir /app
+#ADD . /app
+#WORKDIR /app
+### Add this go mod download command to pull in any dependencies
+#RUN go mod download
+### Our project will now successfully build with the necessary go libraries included.
+#RUN go build -o main .
+### Our start command which kicks off
+### our newly created binary executable
+#CMD ["/app/main"]
--- main.go
+++ main.go
@@ -1,32 +1,41 @@
 package main
 
 import (
-	"crypto/tls"
-	"log"
 	"net/http"
 
 	"golang.org/x/crypto/acme/autocert"
 )
 
 func main() {
-	certManager := autocert.Manager{
-		Prompt:     autocert.AcceptTOS,
-		HostPolicy: autocert.HostWhitelist("hackrpad.com"), //Your domain here
-		Cache:      autocert.DirCache("certs"),             //Folder for storing certificates
-	}
+	//certManager := autocert.Manager{
+	//	Prompt:     autocert.AcceptTOS,
+	//	HostPolicy: autocert.HostWhitelist("hackrpad.com"), //Your domain here
+	//	Cache:      autocert.DirCache("certs"),             //Folder for storing certificates
+	//}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello world"))
 	})
 
-	server := &http.Server{
-		Addr: ":https",
-		TLSConfig: &tls.Config{
-			GetCertificate: certManager.GetCertificate,
-		},
+	//server := &http.Server{
+	//	Addr: ":https",
+	//	TLSConfig: &tls.Config{
+	//		GetCertificate: certManager.GetCertificate,
+	//	},
+	//}
+	//
+	//go http.ListenAndServe(":http", certManager.HTTPHandler(nil))
+	//
+	//log.Fatal(server.ListenAndServeTLS("", "")) //Key and cert are coming from Let's Encrypt
+
+	m := &autocert.Manager{
+		Cache:      autocert.DirCache("certs"),
+		Prompt:     autocert.AcceptTOS,
+		HostPolicy: autocert.HostWhitelist("hackrpad.com", "www.hackrpad.com"),
 	}
-
-	go http.ListenAndServe(":http", certManager.HTTPHandler(nil))
-
-	log.Fatal(server.ListenAndServeTLS("", "")) //Key and cert are coming from Let's Encrypt
+	s := &http.Server{
+		Addr:      ":https",
+		TLSConfig: m.TLSConfig(),
+	}
+	s.ListenAndServeTLS("", "")
 }
